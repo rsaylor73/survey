c8fe16352e6d0709f91c1744b72a53a68cc3ce8b
Robert Saylor <robert@customphpdesign.com>
2016-03-25 17:05:16 -0400

    development

--- index.php
+++ index.php
@@ -5,6 +5,9 @@ include_once('config.php');
 require_once(PATH."/class/loader.class.php");
 $loader = new loader($linkID);
 
+// Header
+$loader->load_smarty($null,'header.tpl');
+
 if ($_GET['section'] != "") {
 	$loader->load_module($_GET['section']);
 } else {
